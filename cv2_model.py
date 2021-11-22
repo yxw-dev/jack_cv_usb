f6d47b769faf86fe2d5ed607debb045dbe9d3fc4
86233 <1341521266@qq.com>
2021-11-22 15:32:27 +0800

    线移动改为像素 ， 匹配调节为异步线程，增加根据结果平移指示线。

--- cv2_model.py
+++ cv2_model.py
@@ -1,6 +1,7 @@
 import cv2
 import os
 import sys
+import time
 
 class cv_match:
     def __init__(self):
@@ -17,20 +18,25 @@ class cv_match:
         self.shuttle = cv2.cvtColor(self.mod2, cv2.COLOR_BGR2RGB)
 
     def get_step(self , img):
-        src = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        res_needle = cv2.matchTemplate(src, self.needle, cv2.TM_SQDIFF)
+        res_needle = cv2.matchTemplate(img, self.needle, cv2.TM_CCOEFF)
+        res_shuttle = cv2.matchTemplate(img, self.shuttle, cv2.TM_CCORR_NORMED)
         min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res_needle)
-        cv2.rectangle(img, (min_loc[0], min_loc[1]), (min_loc[0] + self.width_n, min_loc[1] + self.height_n), (0, 0, 255), 1)
-        res_shuttle = cv2.matchTemplate(src, self.shuttle, cv2.TM_SQDIFF)
+        # cv2.rectangle(img, (max_loc[0], max_loc[1]), (max_loc[0] + self.width_n, max_loc[1] + self.height_n), (0, 0, 255), 1)
         mv, mxv, minl, maxl = cv2.minMaxLoc(res_shuttle)
-        cv2.rectangle(img, (minl[0], minl[1]), (minl[0] + self.width_s, minl[1] + self.height_s), (0, 255, 0), 1)
-        left_line = min_loc[0] + 28
+        # cv2.rectangle(img, (maxl[0], maxl[1]), (maxl[0] + self.width_s, maxl[1] + self.height_s), (0, 255, 0), 1)
+        left_line = max_loc[0] + 28
         right_line = minl[0] - 2
         res = (left_line + right_line) / 2
         step = (right_line - left_line) * 0.022
+        # cv2.imshow('3' , img)
+        # cv2.waitKey()
         return int(res) , step.__abs__()
 
-te = cv_match()
-
-
-
+# tem = cv_match()
+# p =  os.path.dirname(os.path.abspath(sys.argv[0]))
+# img = cv2.imread(p + '\image\id.jpg')
+# t1 = time.time()
+# res = tem.get_step(img)
+# t2 = time.time()
+# diff = t2 - t1
+# print(diff)
